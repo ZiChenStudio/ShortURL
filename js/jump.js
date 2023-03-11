1c3cf714640650241245893da658475e1b825b5e
ZiChenStudio Official <zichenstudio@outlook.com>
2023-03-11 11:40:50 +0800

    Update jump.js

--- js/jump.js
+++ js/jump.js
@@ -2,11 +2,13 @@ var nowPathURL = String(window.location.pathname);
 var jumpHeadURL = 'https://zichenstudio.netlify.app';
 if (nowPathURL != '/0107') {
 	jumpAllURL = String(jumpHeadURL + nowPathURL);
-	function jump() {
-		var jumpText = '<h1>测试: 正常</h1><br><p>即将跳转: <a href="' + jumpAllURL + '">' + jumpAllURL +
-			'</a></p><p>如果浏览器没有自动跳转，请自行点击上方链接跳转。</p>'
-		$('#body').html(jumpText);
+	function jumpURL() {
 		window.location.replace(jumpAllURL);
+	}
+	function jump() {
+		var jumpText = '<h1>测试: 正常</h1><br><p>即将跳转: <a href="' + jumpAllURL + '">' + jumpAllURL + '</a></p><p>如果浏览器没有自动跳转，请自行点击上方链接跳转。</p>'
+		$('#body').html(jumpText);
+		setTimeout('jumpURL()', 1000);
 	};
-	setTimeout('jump()', 5000);
+	setTimeout('jump()', 3000);
 };
